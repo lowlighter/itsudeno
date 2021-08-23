32b8812857d576252ab13d89f7b611e2eadfda7e
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-08-23 22:50:39 +0200

    feat(modules): add log

---
+++ modules/log/all.ts
@@ -0,0 +1,15 @@
+//Imports
+import {Module} from "@generated/modules/log/it.ts"
+import type {before} from "@generated/modules/log/it.ts"
+
+/** Generic implementation */
+Module.register(
+  import.meta.url,
+  class extends Module {
+    /** Apply configuration changes */
+    //deno-lint-ignore require-await
+    async apply({args: {message}}: before) {
+      return {message}
+    }
+  },
+)
