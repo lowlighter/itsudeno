c2ab1867a85b1ad97cbf71abedf9cda589887c7f
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-08-23 22:50:50 +0200

    feat(modules): add net.ping

---
+++ modules/net/ping/all.ts
@@ -0,0 +1,33 @@
+//Imports
+import {Module} from "@generated/modules/net/ping/it.ts"
+import type {before, result} from "@generated/modules/net/ping/it.ts"
+import {run} from "@tools/run"
+import {ItsudenoError} from "@errors"
+
+/** Generic implementation */
+Module.register(
+  import.meta.url,
+  class extends Module {
+    /** Apply configuration changes */
+    async apply({args: {host, packets}}: before) {
+      //Ping target host
+      const {success, stdout, stderr} = await run(`ping -c ${packets} ${host}`)
+      const result = {host, ip: null, transmitted: NaN, received: NaN, loss: NaN} as result
+
+      //Parse result
+      if (success) {
+        const {transmitted, received, loss} = stdout.match(/^(?<transmitted>\d+).+?transmitted, (?<received>\d+).+?received, (?<loss>\d+).+?loss/m)?.groups ?? {}
+        result.ip = stdout.match(/PING .* \((?<ip>[.\da-f:]+)\)/)?.groups?.ip ?? null
+        result.transmitted = Number(transmitted)
+        result.received = Number(received)
+        result.loss = Number(loss) / 100
+        if (result.loss === 1)
+          throw new ItsudenoError.Module("all packets lost")
+      }
+      else {
+        throw new ItsudenoError.Module(`failed to execute:\n${stderr}`)
+      }
+      return result
+    }
+  },
+)
