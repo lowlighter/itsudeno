1dc3d5a252e6aff4a5ea6a708031a2e59daf78e0
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-09-14 20:38:40 -0400

    feat(executors/ssh): add windows implementation

---
+++ executors/ssh/windows.ts
@@ -0,0 +1,32 @@
+//Imports
+import {before, Executor} from "@generated/executors/ssh/it.ts"
+import {is} from "@tools/is"
+import {run} from "@tools/run"
+
+/** Generic implementation */
+Executor.register(
+  import.meta.url,
+  class extends Executor {
+    /** Apply executor */
+    async apply(result: before, payload: string) {
+      const {args: {host, login, password, port, key}} = result
+
+      //Prepare ssh binary
+      let ssh = ""
+      if (password)
+        ssh += `wsl sshpass -p '${password}' `
+      ssh += "ssh -o StrictHostKeyChecking=no "
+
+      //Prepare ssh command
+      ssh += `${host} -l ${login} -p ${port} -T `
+      if (!is.null(key))
+        ssh += `-i ${key}`
+
+      //Prepare command
+      const deno = `'deno run --allow-all --unstable --no-check -'`
+      const command = `${ssh.trim()} ${deno}`
+
+      return this.return(await run(command, {stdin: payload}))
+    }
+  },
+)
