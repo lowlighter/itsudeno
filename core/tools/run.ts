ab55c67df8a81ce7ae587761111239941079ddb6
lowlighter <22963968+lowlighter@users.noreply.github.com>
2021-09-22 22:37:52 -0400

    feat(core\tools): print whether stdin is tty

--- core/tools/run.ts
+++ core/tools/run.ts
@@ -15,8 +15,10 @@ const run = Object.assign(async function run(command: string, {stdin = null, cwd
   try {
     log.vvv(`command: ${command}`)
     process = Deno.run({cmd: argv(command), cwd, env, stdin: stdin ? "piped" : "null", stdout: "piped", stderr: "piped"})
-    if (stdin)
+    if (stdin) {
+      log.vvv(`tty: ${Deno.isatty(process?.stdin)}`)
       await process?.stdin?.write(encoder.encode(stdin))
+    }
     process?.stdin?.close()
     const [{success, code}, stdout, stderr] = await Promise.all([process.status(), process.output(), process.stderrOutput()])
     return {success, code, stdout: decoder.decode(stdout), stderr: decoder.decode(stderr)}
