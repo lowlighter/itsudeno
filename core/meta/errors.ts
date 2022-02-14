6725ed291d8daa11a1dec5140cf0db9eac992bc7
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2022-02-14 04:46:45 +0000

    feat: add `core/meta`

---
+++ core/meta/errors.ts
@@ -0,0 +1,13 @@
+/** Generic itsudeno error */
+export class ItsudenoError extends Error {
+
+  /** Unsupported error */
+  static readonly Unsupported = class ItsudenoUnsupportedError extends ItsudenoError {}
+
+  /** Fatal error */
+  static readonly Fatal = class ItsudenoFatalError extends ItsudenoError {}
+
+  /** Reference error */
+  static readonly Reference = class ItsudenoReferenceError extends ItsudenoError {}
+
+}
---
+++ core/meta/types.ts
@@ -0,0 +1,10 @@
+//deno-lint-ignore no-explicit-any
+export type notyping = any
+
+/** Friend accessor (use this to access private fields) */
+export type friend = notyping
+
+/** Constructor */
+export type constructor<T> = T & {
+  new(...args:unknown[]): T
+}
