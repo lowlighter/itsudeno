41e6d9dc4c33ec022f990162ed5890fd48743a03
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2022-02-14 04:47:21 +0000

    feat: add `core/meta`

--- core/tools/meta/esm.ts
+++
@@ -1,9 +0,0 @@
-//Imports
-import {root} from "core/tools/meta/root.ts"
-import {dirname} from "std/path/mod.ts"
-
-/** ES module path (relative to project root) */
-export function esm(url:string, {dir = false} = {}) {
-  url = root.relative(url)
-  return dir ? dirname(url) : url
-}
